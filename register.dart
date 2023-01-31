dbca4901057f887531e88542aceb195c2f9c11c3
Mikal Paudel <108938487+kalumay@users.noreply.github.com>
2023-01-31 17:25:23 +0545

    Update register.dart

--- register.dart
+++ register.dart
@@ -16,23 +16,35 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Welcome to Flutter',
       home: Scaffold(
-        appBar: AppBar(title: const Text('Welcome to Flutter'),),
+        appBar: AppBar(title: const Text('Welcome to Flutter',textAlign: TextAlign.center),
+        backgroundColor: Colors.black),
         body: SizedBox(
           width: 411,height: 4700, 
           child: SingleChildScrollView(
           child: Column(children: [( const Text('Register Here', style: TextStyle(fontSize:30), )), 
           Image.asset('assests/scooty.jpg',width: 150,height: 150,),
+          
+          //name
           const Padding(
             padding: EdgeInsets.all(15.0),
             child: TextField(decoration: InputDecoration(border: OutlineInputBorder(),hintText: 'Enter your name'),),
           ),
+          
+          //phone
           const Padding(
             padding: EdgeInsets.all(15.0),
             child: TextField(decoration: InputDecoration(border: OutlineInputBorder(),hintText: 'Enter Phone No.'),),
           ),
+          
+          //next
           ElevatedButton(onPressed: ()
-                    {},
-                    child: const Text('Next'), ), Image.asset('assests/screen-shot-2013-11-01-at-2-28-36-pm.png')],
+                    {
+                      Navigator.pushNamed (context,'verify');
+                    },style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black,), 
+                child: const Text('Next',),),
+                    Image.asset('assests/screen-shot-2013-11-01-at-2-28-36-pm.png'), 
+                    ],
                     )
                     )
         
